c2c10c573f3b9d583e16e83e808081e93d47d0dd
Carlo Bruno <carlofbruno@gmail.com>
2024-09-03 15:17:59 -0500

    create Avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,12 @@
+export default function Avatar() {
+  return (
+    <div className="rounded-full">
+      <img
+        src="https://avatar.iran.liara.run/public/1" // 1-100
+        alt="Luis Liara"
+        width={150}
+        height={150}
+      />
+    </div>
+  );
+}
